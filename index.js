7c4e8adba0a69730eafe4a90842ba5cd528da59c
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-05-17 16:33:39 +0600

    i have added here vercel file

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const usersCollection = client.db("userManagementDB").collection("users");
     // send to to client
     app.get("/users", async (req, res) => {
@@ -69,8 +69,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("successfully connected to MongoDB!");
   } finally {
   }
 }
